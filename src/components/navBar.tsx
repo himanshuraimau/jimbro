af967d7303fe64a94058a79e834a0aed2a828178
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-07 22:08:59 +0530

    add nav

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,11 +1,15 @@
+"use client"
 import React from 'react'
 
-const navBar = () => {
+const NavBar = () => {
   return (
     <div>
-      
+         <div className='flex flex-row justify-between p-7 bg-black'>
+                 <div className='text-white text-xl'>JIMBRO</div>
+                <button className='border border-white p-2 rounded-md'>SignUp/SignIn</button>
+         </div>
     </div>
   )
 }
 
-export default navBar
+export default NavBar
