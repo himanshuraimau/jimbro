a032c16a1c74c6c76e30d20842abd6a25f26ba8c
Jishnu <jishnukhargharia11@gmail.com>
2024-07-07 23:39:48 +0530

    add 2nd page

---
+++ src/components/navdash.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navdash() {
+  return (
+    <div>
+         <div className='flex flex-row justify-between p-9 text-white mx-20'>
+                 <div className='text-white text-2xl'>JIMBRO</div>
+                <button className='border border-white p-2 rounded-md text-white'>SignOut</button>
+         </div>
+    </div>
+  )
+}
+
+export default Navdash
