973f08d12822086bbe070511a41b51d1c9230300
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-09 17:38:53 +0530

    add auth minor changes

--- src/components/navdash.tsx
+++ src/components/navdash.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 function Navdash() {
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    signOut({ callbackUrl: '/signin' });
+  };
+
   return (
     <div>
-         <div className='flex flex-row justify-between p-9 text-white mx-20'>
-                 <div className='text-white text-2xl'>JIMBRO</div>
-                <button className='border border-white p-2 rounded-md text-white'>SignOut</button>
-         </div>
+      <div className='flex flex-row justify-between p-9 text-white mx-20'>
+        <div className='text-white text-2xl'>JIMBRO</div>
+        <button 
+          className='border border-white p-2 rounded-md text-white'
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navdash
+export default Navdash;
