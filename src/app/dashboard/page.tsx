8232c8a3324b8c8007e91139451fcc21f0c8c3df
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-09 17:11:46 +0530

    basic signin complete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,22 @@
+import { redirect } from "next/navigation";
 import Navdash from "@/components/navdash";
 import Datescroll from "@/components/datescroll";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/signin");
+    return null;
+  }
+
   return (
-   <>
-
-        <div className="w-screen h-screen bg-black text-white">
-            <Navdash />
-            <Datescroll />
-        </div>
-   
-   </>
+    <div className="w-screen h-screen bg-black text-white">
+      <Navdash />
+      <Datescroll />
+      Welcome {session.user.name}
+    </div>
   );
 }
